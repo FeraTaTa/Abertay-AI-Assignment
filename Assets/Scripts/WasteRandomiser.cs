a2be95e384c22d3b6c5033422aff347f58b1fc22
DESKTOP-2BSF166\Feras <fhathaf@gmail.com>
2019-10-21 20:25:40 +0100

    Initial commit for the project. Includes simple assets and a script which will generate a random number to randomise the placement of "nuclear waste" (path obstacles). Also building a maze for the agent to traverse.

---
+++ Assets/Scripts/WasteRandomiser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasteRandomiser : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rnd = new System.Random();
+        int presence = rnd.Next(1,10);
+        
+        Debug.Log(presence);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
