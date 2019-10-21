addfcefd179e5703650599efbce52a46dc0393d6
DESKTOP-2BSF166\Feras <fhathaf@gmail.com>
2019-10-21 21:30:52 +0100

    Added comment description of script

--- Assets/Scripts/WasteRandomiser.cs
+++ Assets/Scripts/WasteRandomiser.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This script will disable a predefined "nuclear waste" asset 
+ * which acts as a random obstacle in the path of the agent. 
+ * Whether the object is active or not is based on a comparison 
+ * between a developer defined 'ChanceOfActive' and a randomly generated 'presence'
+ */
 public class WasteRandomiser : MonoBehaviour
 {
     [Range(1, 10)]
