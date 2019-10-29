9e6a3a849ef9565716918f7a552b7f5e4880de76
FeraTaTa <fhathaf@gmail.com>
2019-10-29 19:05:06 +0000

    Made the wastes actually random by using Random.Range instead of System which is time-based. Folder restructure. Made some prefabs. Changed the navmesh to take the waste into account, will I need to dynamically bake the mesh at startup??

---
+++ Assets/Scripts/WasteActivator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasteActivator : MonoBehaviour
+{
+    WasteSelector[] nuclearWasteList;
+
+    [Range(1, 10)]
+    public int ChanceOfActive = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nuclearWasteList = GetComponentsInChildren<WasteSelector>(true);
+        Debug.Log("hello");
+        foreach (WasteSelector waste in nuclearWasteList) {
+            
+            
+            float presence = Random.Range(0f, 10f);
+            Debug.Log(presence);
+            if (presence > ChanceOfActive)
+            {
+                waste.transform.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/WasteSelector.cs
+++ Assets/Scripts/WasteSelector.cs
@@ -7,21 +7,12 @@ using UnityEngine;
  * Whether the object is active or not is based on a comparison 
  * between a developer defined 'ChanceOfActive' and a randomly generated 'presence'
  */
-public class WasteRandomiser : MonoBehaviour
+public class WasteSelector : MonoBehaviour
 {
-    [Range(1, 10)]
-    public int ChanceOfActive = 5;
-
     // Start is called before the first frame update
     void Start()
     {
-        var rnd = new System.Random();
-        int presence = rnd.Next(1,10);
         
-        if (presence > ChanceOfActive)
-        {
-            this.transform.gameObject.SetActive(false);
-        }
     }
 
     // Update is called once per frame
