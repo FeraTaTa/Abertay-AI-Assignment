ab25e5d6627472f83067ff4dd862eb851557aa7e
FeraTaTa <fhathaf@gmail.com>
2019-11-03 19:09:58 +0000

    Added a canvas and a button which will toggle the AI method of the agent and display the relevant text in the button. selectorAI references AIControl which is still WIP. Also a bunch of TMP files, I don't know which are actually in use so I committed them all, thanks Microsoft!

---
+++ Assets/Scripts/selectorAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class selectorAI : MonoBehaviour
+{
+    AIControl agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GameObject.FindGameObjectWithTag("AI").GetComponent<AIControl>();
+        updateText();
+    }
+
+    public void toggleActiveAI()
+    {
+        agent.NavMeshActive = !agent.NavMeshActive;
+        updateText();
+    }
+
+    void updateText()
+    {
+        if (agent.NavMeshActive)
+        {
+            this.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "NavMesh Selected";
+        }
+        else
+        {
+            this.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "Rule-Based Selected";
+        }
+    }
+}
