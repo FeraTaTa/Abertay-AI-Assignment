1f20a6ca51a20cd0452d87dd4d09a2bae86eb226
FeraTaTa <fhathaf@gmail.com>
2019-11-04 09:12:38 +0000

    Needed to change the Rule Based target acquisition so that it only selects objects in the PathFinding layer such as the rooms. Previously it was passing the transform of the floor plane, so each mode needed a different call to Physics.Raycast()

--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -6,10 +6,13 @@ public class AgentManager : MonoBehaviour
 {
     public GameObject agent;
     public AIControl agentScript;
+    int layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
+        //select only the 'PathFinding' layer to get a room transform
+        layerMask = 1 << 9;
         agent = GameObject.FindGameObjectWithTag("AI");
         agentScript = agent.GetComponent<AIControl>();
     }
@@ -23,23 +26,24 @@ public class AgentManager : MonoBehaviour
             Ray goalTarget = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
                 
-            if (Physics.Raycast(goalTarget, out hit, 10000))
-            {
                 //send this hit location to the respective AI function
-
                 //NavMesh function
                 if (agentScript.NavMeshActive)
                 {
-                    agentScript.NavAgent.SetDestination(hit.point);
+                    if (Physics.Raycast(goalTarget, out hit, 10000))
+                    {
+                        agentScript.NavAgent.SetDestination(hit.point);
+                    }
                 }
 
                 //Rule Based Function
                 if (!agentScript.NavMeshActive)
                 {
-                    agentScript.agentSearch(hit.transform);
+                    if (Physics.Raycast(goalTarget, out hit, 10000, layerMask))
+                    {
+                        agentScript.agentSearch(hit.transform);
+                    }
                 }
-            }
         }            
     }
-
 }
